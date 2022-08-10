6106cf5a3f1ace6daffd18b756f035bffd732abc
Leeyanhawrt <leeyan1108@gmail.com>
2022-08-10 09:41:03 -0400

    Add stories for when interview slot is empty and full

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -5,8 +5,21 @@ import Show from './Show'
 import Empty from './Empty'
 
 const Appointment = (props) => {
+
+  let interviewScheduled = props.interview ?
+    <Show
+      student={props.interview.student}
+      interviewer={props.interview.interviewer}
+    />
+    : <Empty />
+
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header
+        time={props.time}
+      />
+      {interviewScheduled}
+    </article>
   )
 }
 
