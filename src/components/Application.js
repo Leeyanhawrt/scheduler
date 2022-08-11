d77b2329507033dc6fd8954c9bd5ca95d50c8600
Leeyanhawrt <leeyan1108@gmail.com>
2022-08-10 22:01:45 -0400

    Refactor multiple states into 1

--- src/components/Application.js
+++ src/components/Application.js
@@ -56,8 +56,17 @@ const appointmentDetails = arrayData.map(appointment => {
 })
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday")
-  const [days, setDays] = useState([])
+
+  const setDay = day => setState({ ...state, day })
+  const setDays = days => setState(prev => {
+    return { ...prev, days }
+  })
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  })
 
   useEffect(() => {
     axios.get("/api/days")
@@ -68,6 +77,8 @@ export default function Application(props) {
         console.log(error)
       })
   }, [])
+
+
 
   return (
     <main className="layout">
@@ -80,8 +91,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
           />
         </nav>
